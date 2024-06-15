771519357f2f2a76fa42cf0db8a457cb5ab76f69
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-06-15 16:45:54 +0530

    feat: change supabase code to prisma code in webhook

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -8,6 +8,7 @@ import {
 import authMiddleware from "./middleware/clerk-middleware";
 import supabase from "./lib/supabaseClient";
 import * as dotenv from "dotenv";
+import prisma from "./lib/prismaClient";
 
 dotenv.config();
 
@@ -28,34 +29,58 @@ app.get("/", authMiddleware(), async (req, res) => {
 
 app.post("/clerk-webhook", async (req, res) => {
   const { data, type } = req.body;
-  const { id, email_addresses } = data;
-
+  const { id, email_addresses, first_name, last_name, image_url } = data;
+  const email = email_addresses[0] ? email_addresses[0].email_address : " ";
+  
   console.log(data, type);
+  
+  if (typeof id === "string" && typeof email === "string") {
+    try {
+      if (type === "user.created") {
 
-  try {
-    if (type === "user.created" || type === "user.updated") {
-      const email = email_addresses[0].email_address;
+        const user = await prisma.user.create({
+          data: {
+            id,
+            email,
+            first_name,
+            last_name,
+            image_url,
+          },
+        });
 
-      const { data, error } = await supabase
-        .from("users")
-        .upsert({ id, email }, { onConflict: "id" })
-        .select();
+      }
+      if (type === "user.updated") {
+        const data = await prisma.user.update({
+          where: {
+            id,
+          },
+          data: {
+            email,
+            first_name,
+            last_name,
+            image_url,
+          },
+        });
 
-      if (error) {
-        console.error("Error inserting/updating user in Supabase:", error);
-        return res.status(500).send("Internal Server Error");
+        // const { data, error } = await supabase
+        //   .from("users")
+        //   .upsert({ id, email }, { onConflict: "id" })
+        //   .select();
+
+        
+
+        console.log("User inserted/updated successfully:", data);
       }
 
-      console.log("User inserted/updated successfully:", data);
+      res.sendStatus(200);
+    } catch (err) {
+      console.error("Unexpected error:", err);
+      res.status(500).json("Internal Server Error");
     }
 
     res.sendStatus(200);
-  } catch (err) {
-    console.error("Unexpected error:", err);
-    res.status(500).send("Internal Server Error");
   }
-
-  res.sendStatus(200);
-});
-
-export default app;
+  });
+  
+  export default app;
+  
