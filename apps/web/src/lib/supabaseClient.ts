b373c9c4047f00c071162ef9d2dd7c582573ef05
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-06-29 00:04:02 +0530

    fix: add console.log in client file

--- apps/web/src/lib/supabaseClient.ts
+++ apps/web/src/lib/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
-console.log(supabaseUrl);
+console.log("Supabase Url-->",supabaseUrl);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
