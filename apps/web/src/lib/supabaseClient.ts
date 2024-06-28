f60d8373f936a76a2dee69a2b28972258bb8e428
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-06-28 23:58:19 +0530

    fix: revert supabaseURl in Next_PUBLIC_URL

--- apps/web/src/lib/supabaseClient.ts
+++ apps/web/src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 console.log(supabaseUrl);
 
