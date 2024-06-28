b07cf8a0bdd538dc0cf1b2ef0cb9a0547984f78f
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-06-28 23:50:38 +0530

    feat: add console.log in supabase client

--- apps/web/src/lib/supabaseClient.ts
+++ apps/web/src/lib/supabaseClient.ts
@@ -3,5 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_KEY!;
 
+console.log(supabaseUrl);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
